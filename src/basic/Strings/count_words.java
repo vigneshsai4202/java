1fbb13025ad18e1ba4a0c0e3c323ff8d7d6f9891
Vigesh <vigneshsai378@gmail.com>
2025-07-12 14:44:27 +0530

    init commit

--- src/basic/Strings/count_words.java
+++ src/basic/Strings/count_words.java
@@ -6,7 +6,7 @@ public class count_words {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
-        String[] words=str.split("\\s+");
+        String[] words=str.split(" ");
         System.out.println(words.length);
     }
 }
